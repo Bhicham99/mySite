2ecec019f31ec053c24ed156fd11dae333c9a191
boukaffa <boukaffahicham863@gmail.com>
2024-02-19 11:51:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let img = document.getElementById('image');
+let nom = document.getElementById('nom');
+let disc = document.getElementById('discription');
+let prix = document.getElementById('prix');
+let categorie = document.getElementById('categorie-post');
+
+// let section = document.getElementsByI('section');
+// let computer = document.getElementsByI('computer');
+// let phone = document.getElementsByI('phone');
+// let shoes = document.getElementsByI('shoes');
+// let clothes = document.getElementsByI('clothes');
+
+// function create(){
+//     if(categorie.value === 'computers'){
+//         console.log('categorie.value')
+
+//     }
+    // console.log(categorie.value)
+    
+    
+    // }
+    
+    
+let image = document.getElementById("image");
+console.log(image)
+console.log('image')
+
+
+// function shop(){
+
+// }
